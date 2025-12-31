96705dc5f3f12de3d0a52149788cfc440f8d04a7
VinayKumar018 <kumarvinay15381@gmail.com>
2025-12-31 15:31:05 +0530

    update

--- routes/contact.js
+++ routes/contact.js
@@ -13,5 +13,20 @@ router.get("/", async (req, res) => {
   const data = await Contact.find().sort({ createdAt: -1 });
   res.json(data);
 });
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deleted = await Contact.findByIdAndDelete(id);
+
+    if (!deleted) {
+      return res.status(404).json({ success: false, message: "Not found" });
+    }
+
+    res.json({ success: true, message: "Deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
 
 module.exports = router;
